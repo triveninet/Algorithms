3678db01c613bf0bf608a546f2106a4b7cfd6982
triveninet <triveni.net@gmail.com>
2017-02-22 21:45:08 -0800

    Add files via upload

---
+++ Trees/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        public Node Left
+        {
+            get
+            {
+                return left;
+            }
+
+            set
+            {
+                left = value;
+            }
+        }
+    }
+}
